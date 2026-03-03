a1633f30b0ec1f9872f5de3f3f97ccf18b3ce192
evezeyl <evezeyl@gmail.com>
2026-03-03 16:45:02 +0100

    update

---
+++ libs/viz_factory/src/base.py
@@ -0,0 +1,20 @@
+# viz_factory/src/plot_factory.py
+
+def render_group_plots(group_config, dataframe):
+    """
+    Takes a 'group' from the YAML (e.g., AMR) 
+    and returns a list of Plotly figures.
+    """
+    figures = []
+    
+    for plot_meta in group_config['plots']:
+        fid = plot_meta['factory_id']
+        
+        if fid == "bar_logic":
+            fig = create_bar_chart(dataframe, plot_meta)
+        elif fid == "heatmap_logic":
+            fig = create_heatmap(dataframe, plot_meta)
+        
+        figures.append(fig)
+        
+    return figures
